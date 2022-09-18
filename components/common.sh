a29cfb72d2e8eae5edfb3045d0d62d875889e4fb
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-18 09:30:49 +0530

    updating ./roboshop-shell-script

--- components/common.sh
+++ components/common.sh
@@ -2,7 +2,6 @@ CHECK_ROOT() {
   USER_ID=$(id -u)
   if [ ${USER_ID} -ne 0 ]; then
     echo -e "\e[31m Needed to run the script as root user or add sudo\e[0m"
-    echo -e "\n check log file - ${LOG} for errors\n"
     exit
   fi
 }
@@ -11,6 +10,7 @@ LOG=/tmp/roboshop.log
 rm -f $LOG
 
 CHECK_STAT() {
+  echo -e "\n check log file - ${LOG} for errors\n"
   if [ $? -ne 0 ]; then
     echo -e "\e[31m FAILURE \e[0m"
   else
