aaee12f057ca9752bb368144bf533d886b27db71
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-18 08:32:42 +0530

    updating ./roboshop-shell-script

--- components/frontend.sh
+++ components/frontend.sh
@@ -7,5 +7,5 @@ rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-sed -i -e "/catalogue/ s/localhost/catalogue.roboshop.internal/" -e "/user/ s/localhost/user.roboshop.internal/" -e "/cart/ s/localhost/cart.roboshop.internal/" -e "/shipping/ s/localhost/shipping.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+sed -i -e "/catalogue/ s/localhost/catalogue.roboshop.internal/" -e "/user/ s/localhost/user.roboshop.internal/" -e "/cart/ s/localhost/cart.roboshop.internal/" -e "/shipping/ s/localhost/shipping.roboshop.internal/" -e "/payment/ s/localhost/payment.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
