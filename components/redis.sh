01d1ffde1fc9df47434f9656d950d260d056efaf
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-18 07:30:56 +0530

    updating ./roboshop-shell-script

--- components/redis.sh
+++ components/redis.sh
@@ -1,4 +1,4 @@
-curl -s https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -s -o /etc/yum.repos.d/redis.repo https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo
 yum install redis-6.2.7 -y
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  etc/redis/redis.conf
 systemctl enable redis
