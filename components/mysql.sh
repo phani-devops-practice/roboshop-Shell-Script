e35306df24e930ca516674848173a6d42198f365
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-18 07:54:17 +0530

    updating ./roboshop-shell-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,1 +1,34 @@
-echo Install mysql Component
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+
+## grep temp /var/log/mysqld.log
+#```
+#
+## mysql_secure_installation
+#```
+#
+## mysql -uroot -pRoboShop@1
+#```
+#
+#Once after login to MySQL prompt then run this SQL Command.
+#
+#```sql
+#> uninstall plugin validate_password;
+#```
+#
+### **Setup Needed for Application.**
+#
+#As per the architecture diagram, MySQL is needed by
+#
+#- Shipping Service
+#
+#
+## curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+#```
+#
+## cd /tmp
+## unzip mysql.zip
+## cd mysql-main
+## mysql -u root -pRoboShop@1 <shipping.sql
