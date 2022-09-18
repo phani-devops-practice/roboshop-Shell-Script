5223c0eb8ab19335b29db8f5cc20b893e8b6f54b
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-18 08:14:07 +0530

    updating ./roboshop-shell-script

--- components/shipping.sh
+++ components/shipping.sh
@@ -1,15 +1,15 @@
-#yum install maven -y
-#useradd roboshop
-#cd /home/roboshop
-#curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
-#rm -rf shipping
-#unzip /tmp/shipping.zip
-#mv shipping-main shipping
-#cd /home/roboshop/shipping
-#mvn clean package
-#mv target/shipping-1.0.jar shipping.jar
-#sed -i -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/shipping/systemd.service
-#mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
-#systemctl daemon-reload
-#systemctl start shipping
-#systemctl enable shipping
+yum install maven -y
+useradd roboshop
+cd /home/roboshop
+curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+rm -rf shipping
+unzip /tmp/shipping.zip
+mv shipping-main shipping
+cd /home/roboshop/shipping
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+sed -i -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/shipping/systemd.service
+mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+systemctl start shipping
+systemctl enable shipping
