ebe6b1191426ff444e961155edae1144974342b0
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-18 08:36:57 +0530

    updating ./roboshop-shell-script

--- components/dispatch.sh
+++ components/dispatch.sh
@@ -1,15 +1,16 @@
-#yum install golang -y
-#useradd roboshop
-#curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
-#rm -rf dispatch
-#unzip /tmp/dispatch.zip
-#mv dispatch-main dispatch
-#cd /home/roboshop/dispatch
-#go mod init dispatch
-#go get
-#go build
-#sed -i -e 's/RABBITMQ-IP/rabbitmq.roboshop.internal/' /home/roboshop/dispatch/systemd.service
-#mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
-#systemctl daemon-reload
-#systemctl enable dispatch
-#systemctl start dispatch
+yum install golang -y
+useradd roboshop
+cd /home/roboshop
+curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+rm -rf dispatch
+unzip /tmp/dispatch.zip
+mv dispatch-main dispatch
+cd /home/roboshop/dispatch
+go mod init dispatch
+go get
+go build
+sed -i -e 's/RABBITMQ-IP/rabbitmq.roboshop.internal/' /home/roboshop/dispatch/systemd.service
+mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl start dispatch
