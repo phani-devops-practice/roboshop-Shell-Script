0e00236eb927ca31a553b552d6d5fbda97850905
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-18 10:01:58 +0530

    updating ./roboshop-shell-script

--- components/cart.sh
+++ components/cart.sh
@@ -17,30 +17,28 @@ if [ $? -ne 0 ]; then
   CHECK_STAT $?
 fi
 
-exit
-
-echo "Download cart content"
+PRINT "Download cart content"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>${LOG}
 CHECK_STAT $?
 
 cd /home/roboshop
 
-echo "Remove old content"
+PRINT "Remove old content"
 rm -rf cart &>>${LOG}
 CHECK_STAT $?
 
-echo "Extract cart content"
+PRINT "Extract cart content"
 unzip /tmp/cart.zip &>>${LOG}
 CHECK_STAT $?
 
 mv cart-main cart
 cd /home/roboshop/cart
 
-echo "Install nodejs dependencies"
+PRINT "Install nodejs dependencies"
 npm install &>>${LOG}
 CHECK_STAT $?
 
-echo "Update systemd configuration"
+PRINT "Update systemd configuration"
 sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service &>>${LOG}
 CHECK_STAT $?
 
@@ -49,7 +47,7 @@ mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>${LOG
 systemctl daemon-reload &>>${LOG}
 systemctl enable cart &>>${LOG}
 
-echo "Start cart service"
+PRINT "Start cart service"
 systemctl restart cart &>>${LOG}
 CHECK_STAT $?
 
